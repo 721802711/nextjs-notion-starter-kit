ce043a06737857c738004039c952808da03e235c
没有吃饭 <64204762+721802711@users.noreply.github.com>
2025-04-13 22:08:14 +0800

    Update site.config.ts
    修改个人

--- site.config.ts
+++ site.config.ts
@@ -1,42 +1,42 @@
-import { siteConfig } from './lib/site-config'
+import { siteConfig } from './lib/site-config'               // 引入siteConfig函数
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '748bfc55568049cabf7d46ba218340a3',         // Notion的页面ID
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Next.js Notion Starter Kit',                         // 网站名称
+  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',    // 网站域名
+  author: 'Travis Fischer',                                   // 作者名称
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Example Next.js Notion Starter Kit Site',      // 网站描述
 
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // social usernames (optional)                       
+  twitter: 'transitive_bs',                            // Twitter用户名
+  github: 'transitive-bullshit',                       // GitHub用户名
+  linkedin: 'fisch2',                                   // LinkedIn用户名
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
+  defaultPageIcon: null,                                 // 
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
-  isPreviewImageSupportEnabled: true,
+  isPreviewImageSupportEnabled: true,                          // 是否启用LQIP预览图像支持
 
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
   // environment variables. see the readme for more info
-  isRedisEnabled: false,
+  isRedisEnabled: false,                                        // 是否启用Redis缓存
 
   // map of notion page IDs to URL paths (optional)
   // any pages defined here will override their default URL paths
