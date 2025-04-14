f206f622e781adb323c58b5ddaf5b64f7e1c3c5f
没有吃饭 <64204762+721802711@users.noreply.github.com>
2025-04-15 00:28:15 +0800

    Update site.config.ts
    提交修改

--- site.config.ts
+++ site.config.ts
@@ -2,10 +2,9 @@ import { siteConfig } from './lib/site-config'               // 引入siteConfig
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '748bfc55568049cabf7d46ba218340a3',         // Notion的页面ID
+  rootNotionPageId: '1d52fbfce2a58021a466e2dc10d54457',         // Notion的页面ID
+  // https://www.notion.so/55556666/1d52fbfce2a58021a466e2dc10d54457?pvs=4
 
-  // if you want to restrict pages to a single notion workspace (optional)
-  // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
